efc5c1fce7108d83d6d51e9eafbfe1ea53363ce6
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-09-20 09:51:15 +0200

    images + initial layout + build

--- src/solsort/twister-spinner/twister-spinner.cljs
+++ src/solsort/twister-spinner/twister-spinner.cljs
@@ -15,4 +15,36 @@
    [clojure.string :as string :refer [replace split blank?]]
    [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
 
-(render [:div [:h1 "hello world"]])
+(def colors [:red :green :yellow :blue])
+(def limbs [:left-hand :right-hand :left-foot :right-foot])
+(defn limb [o]
+  (let [size (* 0.45 (min js/innerHeight js/innerWidth))]
+   [:div
+    {:style
+     {:display :inline-block
+      :width size
+      :height size
+      :border-radius (* .5 size)
+      :background (get (apply hash-map (db [:choice])) o :white) 
+      }}
+    [:img
+     {:style
+      {:max-height "100%"
+       :max-width "100%"}
+      :src (str "assets/" (name o) ".png")}]]))
+(defn pick-random! []
+  (db! [:choice] [(rand-nth limbs) (rand-nth colors)]))
+(pick-random!)
+(defonce init
+  (js/setInterval pick-random! 5000))
+(defn main []
+  [:div
+   {:style {:text-align :center}}
+   [limb :left-hand]
+   [limb :right-hand]
+   [:br]
+   [limb :left-foot]
+   [limb :right-foot]
+   ]
+  )
+(render [:div [main]])
